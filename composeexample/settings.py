e252bbdd46dc50b6001e69e3c38890894d2ed436
Pimpisa <boh406@gmail.com>
2016-10-19 01:36:26 +0700

    setting.py

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -76,13 +76,14 @@ WSGI_APPLICATION = 'composeexample.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 """
 DATABASES = {
      'default': {
@@ -93,6 +94,7 @@ DATABASES = {
          'PORT': 5432,
      }
  }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
