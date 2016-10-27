f939b64da64a642b4034a4cc0a519215acedf671
Pimpisa <boh406@gmail.com>
2016-10-27 23:02:07 +0700

    change local db to docker db

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.postgresql_psycopg2',
          'NAME': 'postgres',
          'USER': 'postgres',
-         'HOST': 'db',
+         'HOST': os.getenv("DB_1_PORT_5432_TCP_ADDR"),
          'PORT': 5432,
      }
  }
